d0632f293fc434f1dc9cddbdf3a2a118df0c953c
64010850 Siwakorn Suriya <80116206+jjsornwakii@users.noreply.github.com>
2021-10-05 00:18:54 +0700

    week8

---
+++ String_dotStop2.cpp
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+	int i = 0;
+	struct TEXT{
+		char text[20];
+	};
+	FILE* fp;
+	TEXT str[10000];
+	printf("Input data string : \n");
+	
+	do {
+		scanf("%s", str[i].text);
+		i++;
+	} while (strcmp(str[i-1].text, "."));
+
+	fp = fopen("data.txt", "a");
+	for (int j = 0; j < i; j++) {
+		fprintf(fp ,"%s\n", str[j].text);
+	}
+	fclose(fp);
+	return 0;
+}
